a74e51e2e968d31aeb1d98e186b671b5e83da743
rosebas <sebastianrosales763@gmail.com>
2024-05-01 16:58:45 -0600

    Agrega clase javier

---
+++ src/main/java/umg/Javier.java
@@ -0,0 +1,13 @@
+package umg;
+import java.util.Scanner;
+public class Javier {
+    Scanner scanner = new Scanner(System.in);
+
+    public void hola(){
+        System.out.println("ingresa que javier eres hoy");
+
+        String tipoJavier =  scanner.next();
+
+        System.out.println(tipoJavier);
+    }
+}
