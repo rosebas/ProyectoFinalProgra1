5249a7f840517c9c02a70fa9442d23241d9d4a7d
rosebas <sebasrosales763@gmail.com>
2024-05-02 08:41:23 -0600

    crea el metodo holaTu en la clase Excel Files

--- src/main/java/umg/ExcelFiles.java
+++ src/main/java/umg/ExcelFiles.java
@@ -1,4 +1,18 @@
 package umg;
-
+import java.util.Scanner;
 public class ExcelFiles {
+    Scanner scanner = new Scanner(System.in);
+
+    public void holaTu() {
+        System.out.println("Este cosa la cree para que vieran como deben de usar el git");
+        //Cada linea que agreguen, deben de comentar para ver que hicieron y para que lo hicieron
+        //Por ejenmplo
+        String Javier = "Hola soy javier"; //Variable javier para hacer algo
+        System.out.println(Javier);
+
+        System.out.println("ingresa tu nombre");
+        String nombre = scanner.next();
+
+        System.out.println(nombre);
+    }
 }
