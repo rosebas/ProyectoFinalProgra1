cd1acfcf3d2b7f67d01a5c7ed88e9c83115c08b6
rosebas <sebastianrosales763@gmail.com>
2024-05-05 11:04:26 -0600

    No se que hice

--- src/main/java/umg/ClaseMain.java
+++ src/main/java/umg/ClaseMain.java
@@ -3,11 +3,11 @@ package umg;
 public class ClaseMain {
 
     public static void main(String[] args){
+        //Comentarios xdxdxd gerson 
         //Comentarios xdxdxd gerson
         //Comentarios xdxdxd gerson
         //Comentarios xdxdxd gerson
-        //Comentarios xdxdxd gerson
-        //Comentarios xdxdxd gerson
+        //Comentarios xdxdxd gerso  n
         //Comentarios xdxdxd gerson
         //Comentarios xdxdxd gerson
         //Comentarios xdxdxd gerson
