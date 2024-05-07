c1aecab749746bf1fdeb2748536abd0a904070e6
Gerson <gersongiou@gmail.com>
2024-05-07 12:20:33 -0600

    Cree la interfaz de Apertura de Cuenta

---
+++ src/main/java/umg/AperturaDeCuenta.java
@@ -0,0 +1,13 @@
+package umg;
+
+import javax.swing.*;
+
+public class AperturaDeCuenta {
+    private JPanel panel;
+    private JTextField textField1;
+    private JTextField textField3;
+    private JButton confirmarButton;
+    private JButton noSeXdButton;
+    private JList list1;
+    private JComboBox comboBox1;
+}
