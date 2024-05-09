26ad2b56ba4e707614ebd24d37765fca5926d9d9
soyJavier <javier.fajardo098@gmail.com>
2024-05-08 20:05:32 -0600

    se realizo la creacion de las clases RetirarDinero y DepositarDinero

---
+++ src/main/java/umg/RealizarDeposito.java
@@ -0,0 +1,10 @@
+package umg;
+
+import javax.swing.*;
+
+public class RealizarDeposito {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton depositarButton;
+}
---
+++ src/main/java/umg/RetirarDinero.java
@@ -0,0 +1,9 @@
+package umg;
+
+import javax.swing.*;
+
+public class RetirarDinero {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton retirarButton;
+}
