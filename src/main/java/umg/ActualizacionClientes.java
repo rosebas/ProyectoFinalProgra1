b6dbd7c3e8ad0691679bfd2856962f4094df3f40
rosebas <sebastianrosales763@gmail.com>
2024-05-16 22:26:53 -0600

    Eliminar clases de IntelliJ para cambiar a trabajar en NetBeans desde cero

--- src/main/java/umg/ActualizacionClientes.java
+++
@@ -1,2 +0,0 @@
-package umg;public class ActualizacionClientes {
-}
--- src/main/java/umg/RetirarDinero.java
+++
@@ -1,9 +0,0 @@
-package umg;
-
-import javax.swing.*;
-
-public class RetirarDinero {
-    private JTextField textField1;
-    private JTextField textField2;
-    private JButton retirarButton;
-}
