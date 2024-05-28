c4b8cfcdef14b2a57c8531d31f9b328cc87f87e2
rosebas <sebasrosales763@gmail.com>
2024-05-28 10:56:08 -0600

    Corrige Errores eliminando el form de apertura de cuenta

--- src/main/java/umg/MenuPrincipal.java
+++ src/main/java/umg/MenuPrincipal.java
@@ -156,9 +156,7 @@ public class MenuPrincipal extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-      AperturaCuenta aperturaCuenta = new AperturaCuenta();
-       this.setVisible(false);
-       aperturaCuenta.setVisible(true);
+      
        
     }//GEN-LAST:event_jButton3ActionPerformed
 
